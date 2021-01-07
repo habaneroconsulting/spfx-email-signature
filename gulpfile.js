d7564cc2588cf084f179f506fd8b49630c630fee
Christopher Parsons <cwlparsons@gmail.com>
2021-01-07 12:08:28 -0500

    Add webpack stats generation to production builds

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,34 @@
 
 const build = require('@microsoft/sp-build-web');
 
-// Force use of projects specified react version.
-// @see https://inprod.dev/blog/2020-02-12-spfx-sharepoint-server-2019/
-// Necessary for `react-testing-library` to use `waitFor utilities, which require `react-dom@16.9.0`.
 build.configureWebpack.mergeConfig({
   additionalConfiguration: (generatedConfiguration) => {
+    // When creating a production build...
+    if (process.argv.indexOf('--ship') > -1 || process.argv.indexOf('-s') > -1) {
+      // Create a stats folder and put generated webpack stats within it.
+      const path = require('path');
+      const bundleAnalyzer = require('webpack-bundle-analyzer');
+
+      const lastDirName = path.basename(__dirname);
+      const dropPath = path.join(__dirname, 'temp', 'stats');
+
+      generatedConfiguration.plugins.push(
+        new bundleAnalyzer.BundleAnalyzerPlugin({
+          openAnalyzer: false,
+          analyzerMode: 'static',
+          reportFilename: path.join(dropPath, `${lastDirName}.stats.html`),
+          generateStatsFile: true,
+          statsFilename: path.join(dropPath, `${lastDirName}.stats.json`),
+          logLevel: 'error'
+        })
+      );
+    }
+
+    // Force use of projects specified react version.
+    // @see https://inprod.dev/blog/2020-02-12-spfx-sharepoint-server-2019/
+    // Necessary for `react-testing-library` to use `waitFor utilities, which require `react-dom@16.9.0`.
     generatedConfiguration.externals = generatedConfiguration.externals.filter((name) => !['react', 'react-dom'].includes(name));
+
     return generatedConfiguration;
   }
 });
