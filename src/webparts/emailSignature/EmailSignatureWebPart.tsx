a15bf632ae0081f813d37e01d39cf5fbc9693b14
Christopher Parsons <786967+cwparsons@users.noreply.github.com>
2023-06-13 17:01:21 -0700

    Refactor property pane out into separate lazy loaded component

--- src/webparts/emailSignature/EmailSignatureWebPart.tsx
+++ src/webparts/emailSignature/EmailSignatureWebPart.tsx
@@ -3,15 +3,14 @@ import ReactDom from 'react-dom';
 
 import { IReadonlyTheme, ThemeChangedEventArgs, ThemeProvider } from '@microsoft/sp-component-base';
 import { Environment, EnvironmentType } from '@microsoft/sp-core-library';
-import { IPropertyPaneConfiguration, PropertyPaneTextField, PropertyPaneToggle } from '@microsoft/sp-property-pane';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 
-import * as strings from 'EmailSignatureWebPartStrings';
 import { EmailSignature } from './components/EmailSignature/EmailSignature';
 import { EmailSignatureService } from './services/EmailSignatureService';
 import { IEmailSignatureService } from './services/IEmailSignatureService';
 import { LocalEmailSignatureService } from './services/LocalEmailSignatureService';
 import { EmailSignatureWebPartProps } from './types';
+import type { EmailSignatureWebPartPropertyPane } from './EmailSignatureWebPartPropertyPane';
 
 export default class EmailSignatureWebPart extends BaseClientSideWebPart<EmailSignatureWebPartProps> {
   private _emailSignatureService: IEmailSignatureService;
@@ -19,10 +18,7 @@ export default class EmailSignatureWebPart extends BaseClientSideWebPart<EmailSi
   private _themeVariant?: IReadonlyTheme | undefined;
 
   // Dynamically loaded property fields.
-  private _CustomCollectionFieldType: any;
-  private _PropertyFieldCodeEditor: any;
-  private _PropertyFieldCollectionData: any;
-  private _PropertyFieldNumber: any;
+  private _DeferredPropertyPane: EmailSignatureWebPartPropertyPane;
 
   /**
    * Get theme, Microsoft Graph values.
@@ -59,120 +55,25 @@ export default class EmailSignatureWebPart extends BaseClientSideWebPart<EmailSi
     );
   }
 
-  /**
-   * Dynamically load code editor, collection data editor when the web part edit panel opens.
-   */
   protected async loadPropertyPaneResources() {
-    const { PropertyFieldCodeEditor } = await import(
-      /* webpackChunkName: 'emailsignature-propertyfieldcodeeditor' */
-      '@pnp/spfx-property-controls/lib/PropertyFieldCodeEditor'
+    super.loadPropertyPaneResources();
+
+    const component = await import(
+      /* webpackChunkName: 'EmailSignatureWebPartPropertyPane'*/
+      './EmailSignatureWebPartPropertyPane'
     );
 
-    this._PropertyFieldCodeEditor = PropertyFieldCodeEditor;
-
-    const { CustomCollectionFieldType, PropertyFieldCollectionData } = await import(
-      /* webpackChunkName: 'emailsignature-propertyfieldcollectiondata' */
-      '@pnp/spfx-property-controls/lib/PropertyFieldCollectionData'
-    );
-
-    this._CustomCollectionFieldType = CustomCollectionFieldType;
-    this._PropertyFieldCollectionData = PropertyFieldCollectionData;
-
-    const { PropertyFieldNumber } = await import(
-      /* webpackChunkName: 'emailsignature-propertyfieldnumber' */
-      '@pnp/spfx-property-controls/lib/PropertyFieldNumber'
-    );
-
-    this._PropertyFieldNumber = PropertyFieldNumber;
+    this._DeferredPropertyPane = new component.EmailSignatureWebPartPropertyPane();
   }
 
-  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
-    return {
-      pages: [
-        {
-          header: {
-            description: strings.PropertyPaneDescription
-          },
-          groups: [
-            {
-              groupName: strings.ContentGroupName,
-              groupFields: [
-                PropertyPaneTextField('webPartTitleText', {
-                  label: strings.WebPartTitleFieldLabel
-                }),
-                this._PropertyFieldCodeEditor('htmlTemplate', {
-                  initialValue: this.properties.htmlTemplate,
-                  key: 'HtmlTemplateFieldKey',
-                  label: strings.HtmlTemplateFieldLabel,
-                  language: 'html', // PropertyFieldCodeEditorLanguages.HTML
-                  onPropertyChange: this.onPropertyPaneFieldChanged.bind(this),
-                  panelTitle: strings.HtmlTemplateFieldLabel,
-                  properties: this.properties
-                }),
-                this._PropertyFieldCollectionData('customProperties', {
-                  disabled: false,
-                  fields: [
-                    {
-                      id: 'key',
-                      title: strings.KeyCollectionLabel,
-                      type: this._CustomCollectionFieldType.string,
-                      required: true
-                    },
-                    {
-                      id: 'label',
-                      title: strings.LabelCollectionLabel,
-                      type: this._CustomCollectionFieldType.string,
-                      required: true
-                    },
-                    {
-                      id: 'value',
-                      title: strings.ValueCollectionLabel,
-                      type: this._CustomCollectionFieldType.string,
-                      required: false
-                    },
-                    {
-                      id: 'editable',
-                      title: strings.UserEditableCollectionLabel,
-                      type: this._CustomCollectionFieldType.boolean
-                    }
-                  ],
-                  key: 'CustomPropertiesFieldKey',
-                  label: strings.CustomPropertyCollectionLabel,
-                  manageBtnLabel: strings.CustomPropertyManageButton,
-                  panelHeader: strings.CustomPropertyCollectionLabel,
-                  value: this.properties.customProperties
-                })
-              ]
-            },
-            {
-              groupName: 'Image details',
-              groupFields: [
-                PropertyPaneToggle('addCircleMask', {
-                  label: strings.AddCircleMaskFieldLabel
-                }),
-                this._PropertyFieldNumber('imageSize', {
-                  key: 'PropertyFieldNumberKey',
-                  label: strings.ImageSizeFieldLabel,
-                  minValue: 1,
-                  value: this.properties.imageSize
-                })
-              ]
-            },
-            {
-              groupName: 'Settings',
-              groupFields: [
-                PropertyPaneToggle('enableEditing', {
-                  label: strings.EnableEditingFieldLabel
-                }),
-                PropertyPaneToggle('forceLowercaseEmails', {
-                  label: strings.ForceLowercaseEmailsFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
-    };
+  protected getPropertyPaneConfiguration() {
+    return this._DeferredPropertyPane.getPropertyPaneConfiguration({
+      context: this.context,
+      onPropertyChange: (propertyPath, oldValue, newValue) => {
+        this.onPropertyPaneFieldChanged(propertyPath, oldValue, newValue);
+      },
+      properties: this.properties
+    });
   }
 
   protected onDispose(): void {
---
+++ src/webparts/emailSignature/EmailSignatureWebPartPropertyPane.tsx
@@ -0,0 +1,106 @@
+import { PropertyPaneTextField, type IPropertyPaneConfiguration, PropertyPaneToggle } from '@microsoft/sp-property-pane';
+import type { WebPartContext } from '@microsoft/sp-webpart-base';
+import { PropertyFieldCodeEditor, PropertyFieldCodeEditorLanguages } from '@pnp/spfx-property-controls/lib/PropertyFieldCodeEditor';
+import { PropertyFieldCollectionData, CustomCollectionFieldType } from '@pnp/spfx-property-controls/lib/PropertyFieldCollectionData';
+
+import { EmailSignatureWebPartProps } from './types';
+import { PropertyFieldNumber } from '@pnp/spfx-property-controls/lib/PropertyFieldNumber';
+
+import * as strings from 'EmailSignatureWebPartStrings';
+
+type WebPartPropertyPaneProps = {
+  context: WebPartContext;
+  onPropertyChange: (propertyPath: string, oldValue: any, newValue: any) => void;
+  properties: EmailSignatureWebPartProps;
+};
+
+export class EmailSignatureWebPartPropertyPane {
+  public getPropertyPaneConfiguration({ onPropertyChange, properties }: WebPartPropertyPaneProps): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.ContentGroupName,
+              groupFields: [
+                PropertyPaneTextField('webPartTitleText', {
+                  label: strings.WebPartTitleFieldLabel
+                }),
+                PropertyFieldCodeEditor('htmlTemplate', {
+                  initialValue: properties.htmlTemplate,
+                  key: 'HtmlTemplateFieldKey',
+                  label: strings.HtmlTemplateFieldLabel,
+                  language: PropertyFieldCodeEditorLanguages.HTML,
+                  onPropertyChange: onPropertyChange,
+                  panelTitle: strings.HtmlTemplateFieldLabel,
+                  properties: properties
+                }),
+                PropertyFieldCollectionData('customProperties', {
+                  disabled: false,
+                  fields: [
+                    {
+                      id: 'key',
+                      title: strings.KeyCollectionLabel,
+                      type: CustomCollectionFieldType.string,
+                      required: true
+                    },
+                    {
+                      id: 'label',
+                      title: strings.LabelCollectionLabel,
+                      type: CustomCollectionFieldType.string,
+                      required: true
+                    },
+                    {
+                      id: 'value',
+                      title: strings.ValueCollectionLabel,
+                      type: CustomCollectionFieldType.string,
+                      required: false
+                    },
+                    {
+                      id: 'editable',
+                      title: strings.UserEditableCollectionLabel,
+                      type: CustomCollectionFieldType.boolean
+                    }
+                  ],
+                  key: 'CustomPropertiesFieldKey',
+                  label: strings.CustomPropertyCollectionLabel,
+                  manageBtnLabel: strings.CustomPropertyManageButton,
+                  panelHeader: strings.CustomPropertyCollectionLabel,
+                  value: properties.customProperties
+                })
+              ]
+            },
+            {
+              groupName: 'Image details',
+              groupFields: [
+                PropertyPaneToggle('addCircleMask', {
+                  label: strings.AddCircleMaskFieldLabel
+                }),
+                PropertyFieldNumber('imageSize', {
+                  key: 'PropertyFieldNumberKey',
+                  label: strings.ImageSizeFieldLabel,
+                  minValue: 1,
+                  value: properties.imageSize
+                })
+              ]
+            },
+            {
+              groupName: 'Settings',
+              groupFields: [
+                PropertyPaneToggle('enableEditing', {
+                  label: strings.EnableEditingFieldLabel
+                }),
+                PropertyPaneToggle('forceLowercaseEmails', {
+                  label: strings.ForceLowercaseEmailsFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
