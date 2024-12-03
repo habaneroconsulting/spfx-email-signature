e3b6a45154ce3aab9214855cd25f86cd6b75b635
Christopher Parsons <786967+cwparsons@users.noreply.github.com>
2024-12-03 14:22:21 -0800

    Fix multiple lines in web part title editing textarea

--- src/webparts/emailSignature/components/EmailSignatureWebPartTitle/EmailSignatureWebPartTitle.tsx
+++ src/webparts/emailSignature/components/EmailSignatureWebPartTitle/EmailSignatureWebPartTitle.tsx
@@ -2,29 +2,101 @@
 
 import { jsx } from '@emotion/react';
 import { DisplayMode } from '@microsoft/sp-core-library';
+import { useRef, useState, useEffect } from 'react';
 
 import { EmailSignatureWebPartProps } from '../../types';
+import strings from 'EmailSignatureWebPartStrings';
 
 type EmailSignatureWebPartTitleProps = Pick<EmailSignatureWebPartProps, 'webPartTitleText'> & {
   displayMode: DisplayMode;
   updateWebPartTitleText: (value: string) => void;
 };
 
-export const EmailSignatureWebPartTitle = ({ displayMode, updateWebPartTitleText, webPartTitleText }: EmailSignatureWebPartTitleProps) => {
-  return (
-    <div css={{ marginBottom: 24 }}>
-      {webPartTitleText && displayMode === DisplayMode.Read && (
-        <h2 css={{ fontSize: 20, fontWeight: 600, margin: 0, minHeight: 23 }}>{webPartTitleText}</h2>
-      )}
+export const EmailSignatureWebPartTitle = ({
+  displayMode = DisplayMode.Read,
+  updateWebPartTitleText,
+  webPartTitleText
+}: EmailSignatureWebPartTitleProps) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
-      {displayMode === DisplayMode.Edit && (
-        <textarea
-          css={{ border: 0, fontSize: 20, fontWeight: 600, margin: 0, minHeight: 23, padding: 0, resize: 'none' }}
-          defaultValue={webPartTitleText}
-          onChange={(e) => updateWebPartTitleText(e.target.value)}
-          rows={1}
-        />
-      )}
+  const [webPartTitle, setWebPartTitle] = useState(webPartTitleText);
+
+  /**
+   * On text changes, run the update property callback and then re-calculate
+   * how tall the textarea should be.
+   */
+  const onTextareaChange = (textarea: HTMLTextAreaElement) => {
+    if (updateWebPartTitleText) {
+      const value = textarea.value;
+
+      updateWebPartTitleText(value);
+      setWebPartTitle(value);
+    }
+
+    textarea.style.removeProperty('height');
+    textarea.style.height = `${textarea.scrollHeight}px`;
+  };
+
+  useEffect(() => {
+    setWebPartTitle(webPartTitleText);
+  }, [webPartTitleText]);
+
+  // Don't render anything if in read mode and title is empty
+  if (displayMode === DisplayMode.Read && (!webPartTitleText || webPartTitleText.trim().length === 0)) {
+    return null;
+  }
+
+  return (
+    <div
+      css={{
+        alignItems: 'baseline',
+        display: 'flex'
+      }}
+    >
+      <div
+        css={{
+          flexGrow: 1,
+          marginBlockEnd: '12px',
+          marginInlineEnd: '32px'
+        }}
+      >
+        {webPartTitle && (displayMode === DisplayMode.Read || !updateWebPartTitleText) && (
+          <h2
+            css={{
+              fontWeight: 600,
+              marginBlock: 0,
+              minHeight: 23
+            }}
+          >
+            {webPartTitle}
+          </h2>
+        )}
+
+        {displayMode === DisplayMode.Edit && updateWebPartTitleText && (
+          <textarea
+            aria-label={strings.WebPartTitleFieldLabel}
+            css={{
+              background: 'transparent',
+              border: 0,
+              fontFamily:
+                "'Segoe UI','Segoe UI Web (West European)','Segoe UI',-apple-system,BlinkMacSystemFont,Roboto,'Helvetica Neue',sans-serif",
+              fontSize: 20,
+              fontWeight: 600,
+              label: 'web-part-title-textarea',
+              lineHeight: 1.2,
+              height: '1.2em',
+              margin: 0,
+              overflowY: 'hidden',
+              padding: 0,
+              resize: 'none',
+              width: '100%'
+            }}
+            onChange={(e) => onTextareaChange(e.currentTarget)}
+            ref={textareaRef}
+            value={webPartTitle}
+          />
+        )}
+      </div>
     </div>
   );
 };
